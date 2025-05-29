6ae32df38157067945aaed1518e8bc78287d76b2
emily.augusto <emilyaugusto4@gmail.com>
2025-05-29 15:20:00 -0300

    feat

---
+++ AppMAUIGallery/Models/Component.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppMAUIGallery.Models
+{
+    internal class Component
+    {
+        public string Title { get; set; } // Nome do componente
+        public string Description { get; set; } // Descrição do componente
+        public Page Page { get; set; } // Página do componente
+    }
+}
